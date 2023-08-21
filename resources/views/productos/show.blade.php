5b28487cbf847071c5de49af88fdbd01583e9c27
reynaldo29 <acm96120@gmail.com>
2023-08-21 16:05:35 -0500

    adding some changes

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2> Mostrar productos</h2>
+                <h2> Show productos</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('productos.index') }}"> Back</a>
